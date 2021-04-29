65ed783a08c9f7bc04d7cb0ba2632f8c4915f452
Barbara Kowalska <kowalskab389@gmail.com>
2021-04-29 12:16:04 +0200

    added basic class schema

---
+++ src/Farm.java
@@ -0,0 +1,14 @@
+/*
+* farma
+	- ziemia w hektarach
+	- budynki
+	- zwierzęta - gracz może hodować
+	- uprawy - gracz może uprawiać
+	- zbiory
+		- sprzedać
+		- przechowywać
+		- używać do karmienia zwierząt
+*/
+
+public class Farm {
+}
---
+++ src/Plant.java
@@ -0,0 +1,17 @@
+/*
+rośliny
+	* sadzonki
+	* nasiona
+	- koszt zakupu
+	- koszt przygotowania ziemi i sadzenia (/hektar)
+	- koszt ochrony przed szkodnikami (/hektar)
+	- wydajnoć (zbiory w tona/hektar)
+	- czas od posadzenia do zbioru (w tyg)
+	- informację kiedy można sadzić (tyg w roku)
+	- koszt zbioru (/hektar)
+	- cena skupu (/kg)
+	- miejsce do przechowywania
+	- max czas przechowywania
+* */
+public class Plant {
+}
